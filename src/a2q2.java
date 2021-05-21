4b164e8f8c5c6216257815b4e638cb9bbec979e3
Allen Keettikkal <allenkeettikkal@gmail.com>
2021-05-21 09:01:24 -0600

    Added Console Output and While Conditionals to Assignment

--- src/a2q2.java
+++ src/a2q2.java
@@ -4,6 +4,25 @@
     Student Number: 11278995
  */
 
+import java.util.Scanner;
+
 public class a2q2 {
 
+    public static void main(String []args){
+        height("Starting Height: ");
+    }
+
+    public static void height(String height){
+
+        Scanner in = new Scanner(System.in);
+        System.out.print(height);
+        int num = Integer.parseInt(in.nextLine());
+
+        while (num > 1){
+            if ( num % 2 == 0 )
+                num = num / 2;
+            else
+                num = 3 * num + 1;
+        }
+    }
 }
